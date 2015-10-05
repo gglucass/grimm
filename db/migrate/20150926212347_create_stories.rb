d399143edf5026a926868dd60d2afe1d12ffafce
Garm Lucassen <garm.lucassen@gmail.com>
2015-10-05 10:11:36 +0200

    Add foreign key so DB is defined in the same way as for Flask

--- db/migrate/20150926212347_create_stories.rb
+++ db/migrate/20150926212347_create_stories.rb
@@ -7,5 +7,6 @@ class CreateStories < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_foreign_key :stories, :projects
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926212347) do
+ActiveRecord::Schema.define(version: 20151003142250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,4 +76,5 @@ ActiveRecord::Schema.define(version: 20150926212347) do
   add_index "users", ["integration_id"], name: "index_users_on_integration_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "stories", "projects"
 end
