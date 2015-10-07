a8f70ddc74a42ec3939f5349626a08ddadfcfe82
Garm Lucassen <garm.lucassen@gmail.com>
2015-10-07 21:15:10 +0200

    Don't screw up if you have multiple integrations!

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,15 +6,16 @@ class Comment < ActiveRecord::Base
   before_destroy :remove_externally
 
   def remove_externally
-    self.send("remove_from_#{self.user.integrations.first.kind}")
+    integration = self.user.integrations.where(kind: self.defect.project.kind).first
+    self.send("remove_from_#{integration.kind}", integration)
   end
 
-  def remove_from_pivotal
-    HTTP.headers('X-TrackerToken' => self.user.integrations.where(kind: 'pivotal').first.auth_info).delete("https://www.pivotaltracker.com/services/v5/projects/#{self.defect.project.external_id}/stories/#{self.defect.story.external_id}/comments/#{self.external_id}")
+  def remove_from_pivotal(integration)
+    HTTP.headers('X-TrackerToken' => integration.auth_info).delete("https://www.pivotaltracker.com/services/v5/projects/#{self.defect.project.external_id}/stories/#{self.defect.story.external_id}/comments/#{self.external_id}")
   end
 
-  def remove_from_jira
-    integration = self.user.integrations.where(kind: 'jira').first
+  def remove_from_jira(integration)
     HTTP.basic_auth(user: integration.auth_info["jira_username"], pass: integration.auth_info["jira_password"]).delete("#{integration.auth_info["jira_url"]}/rest/api/2/issue/#{self.defect.story.external_id}/comment/#{self.external_id}")
+    HTTP.basic_auth(user: integration.auth_info["jira_username"], pass: integration.auth_info["jira_password"]).delete("#{integration.auth_info["jira_url"]}/rest/api/2/issue/#{comment.defect.story.external_id}/comment/#{comment.external_id}")
   end
 end
