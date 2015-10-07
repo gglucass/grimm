df5a265ce76e4a4e5559feedffe548b3be0ca7c4
Garm Lucassen <garm.lucassen@gmail.com>
2015-10-07 14:19:15 +0200

    Better defect messages

--- app/models/defect.rb
+++ app/models/defect.rb
@@ -8,7 +8,9 @@ class Defect < ActiveRecord::Base
     client = TrackerApi::Client.new(token: user.integrations.first.auth_info)
     project = client.project(self.project.external_id)
     story = project.story(self.story.external_id)
-    response = client.post("/projects/#{project.id}/stories/#{story.id}/comments", params: {text: self.highlight})
+    text = "__" + I18n.t("defect_comments.#{self.kind}.#{self.subkind}") + "__\n" + I18n.t("defect_comments.#{self.kind}.#{self.subkind}_explanation") + "\n" + "__Suggestion__: " + self.highlight
+
+    response = client.post("/projects/#{project.id}/stories/#{story.id}/comments", params: {text: text})
     if response
       self.comments.create(external_id: response.body['id'], text: response.body['text'], user: user)
     end
