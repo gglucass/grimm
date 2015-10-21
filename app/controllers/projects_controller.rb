781b7e207521d3534d2c93622a177d5e9fd756a8
Garm Lucassen <garm.lucassen@gmail.com>
2015-10-21 19:58:03 +0200

    Add project#show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,13 @@ class ProjectsController < ApplicationController
   before_action :set_and_authorize_project, only: [:show]
   
   def show
+    @project_defects = @project.defects.where(false_positive: false)
+    @severe_defects = @project.defects.where(severity: "medium", false_positive: false)
+    @medium_defects = @project.defects.where(severity: "high", false_positive: false)
+    @minor_defects = @project.defects.where(severity: "minor", false_positive: false)
+    @false_positives = @project.defects.where(false_positive: true)
+    @perfect_stories = Story.includes(:defects).where(defects: {id: nil}, project_id: @project.id)
+    @stories = @project.stories.sort
   end
 
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 # id, name, external_id
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :users, -> { uniq }
+  has_many :defects
   has_many :stories, dependent: :destroy
 
   def analyze
