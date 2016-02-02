f2698052fb1ef89c5ddac8f55a1a77acc55c60da
Garm Lucassen <garm.lucassen@gmail.com>
2016-02-02 16:06:12 +0100

    Set whodunnit for every version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :set_paper_trail_whodunnit
 end
