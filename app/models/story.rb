0072deb330fd2f6331de8fa6392a29000c2a48bc
Garm Lucassen <garm.lucassen@gmail.com>
2016-02-08 16:53:56 +0100

    Validate uniqueness of external_id within a project

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,7 @@ class Story < ActiveRecord::Base
   has_paper_trail
   belongs_to :project
   has_many :defects, dependent: :destroy
+  validates_uniqueness_of :external_id, scope: [:project_id]
 
   def analyze
     Defect.where(story_id: self.id, false_positive: false).each do |defect|
