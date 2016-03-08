9f5fe5af16b1a6505f29fa9f7c6a3e4654726b66
Garm Lucassen <garm.lucassen@gmail.com>
2016-03-08 17:56:32 +0100

    paginate issues

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -52,18 +52,26 @@ class Integration < ActiveRecord::Base
       new_project.integrations += [self]
       new_record_project = new_project.new_record?
       new_project.save()
-      issues = project.issues.select { |i| i.issuetype.name.in?(['Story', new_project.custom_issue_type]) }
-      issues.each do |issue|
-        new_story = new_project.stories.find_or_initialize_by(external_id: issue.id, title: issue.summary) 
-        if new_story.new_record?
-          new_story.save()
-          new_story.analyze()
-        end
-        new_story.update_attributes(priority: issue.priority.name, status: issue.status.name, comments: issue.comments.to_json, 
-          description: issue.description, external_key: issue.key)
-        if issue.try(:customfield_10008)
-          new_story.update_attributes(estimation: issue.customfield_10008)
-        end
+      continue = true
+      start_at = 0
+      while continue
+        issues = project.issues(startAt: start_at, maxResults: 100).select { |i| i.issuetype.name.in?(['Story', new_project.custom_issue_type]) }
+        continue = issues.count == 100
+        start_at += 100
+        Thread.start {
+          issues.each do |issue|
+            new_story = new_project.stories.find_or_initialize_by(external_id: issue.id, title: issue.summary) 
+            if new_story.new_record?
+              new_story.save()
+              new_story.analyze()
+            end
+            new_story.update_attributes(priority: issue.priority.name, status: issue.status.name, comments: issue.comments.to_json, 
+              description: issue.description, external_key: issue.key)
+            if issue.try(:customfield_10008)
+              new_story.update_attributes(estimation: issue.customfield_10008)
+            end
+          end
+        }
       end
       if new_record_project
         new_project.reload.analyze(first_analysis: false)
