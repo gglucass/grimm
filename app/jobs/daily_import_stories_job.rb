2e4209e1573a4dbdda856103c281b6dabbc78eda
Garm Lucassen <garm.lucassen@gmail.com>
2016-03-24 15:57:57 +0100

    Very hacking way to only  import stories for coolblue on coolblue server

--- app/jobs/daily_import_stories_job.rb
+++ app/jobs/daily_import_stories_job.rb
@@ -2,7 +2,12 @@ class DailyImportStoriesJob < ActiveJob::Base
   queue_as :default
 
   def perform()
-    Integration.where(kind: 'jira').each do |integration|
+    if ENV["HOSTNAME"] == "https://aqusa-coolblue.science.uu.nl"
+      integrations = Integration.where(kind: 'jira', site_url: 'jira.coolblue.eu')
+    else
+      integrations = Integration.where(kind: 'jira').where_not(site_url: 'jira.coolblue.eu')
+    end
+    integrations.each do |integration|
       client = integration.initialize_jira_client
       integration.initialize_jira_stories(client, [])
     end
