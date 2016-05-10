e88d46f05ce3996136b128a9e2d0d233b06f0f37
Garm Lucassen <garm.lucassen@gmail.com>
2016-05-10 14:30:49 +0200

    Daily calculate sprint stats

--- app/jobs/daily_calculate_sprint_stats_job.rb
+++ app/jobs/daily_calculate_sprint_stats_job.rb
@@ -4,16 +4,19 @@ class DailyCalculateSprintStatsJob < ActiveJob::Base
   def perform()
     integrations = Integration.get_all_integrations()
     integrations.each do |integration|
-      integration.projects.each do |project|
-        Board.import_boards(integration, project)
-        project.boards.each do |board|
-          Sprint.import_sprints(integration, project, board)
-          board.sprints.each do |sprint|
-            if sprint.recidivism_rate.nil? or sprint.recidivism_rate.nan? or sprint.end_date > (Date.today-30)
-              sprint.calculate_recidivism_rate(integration, project, board)
-              sprint.save()
+      begin
+        integration.projects.each do |project|
+          Board.import_boards(integration, project)
+          project.boards.each do |board|
+            Sprint.import_sprints(integration, project, board)
+            board.sprints.each do |sprint|
+              if sprint.recidivism_rate.nil? or sprint.recidivism_rate.nan? or sprint.end_date > (Date.today-30)
+                sprint.calculate_recidivism_rate(integration, project, board)
+                sprint.save()
+              end
             end
           end
+        rescue
         end
       end
     end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
 every 1.day, :at => '1:00 am' do
   runner "DailyImportStoriesJob.perform_later()"
 end
+every 1.day, :at => '3:00 am' do
+  runner "DailyCalculateSprintStatsJob.perform_later()"
+end
